14722db3069938bfdc262fc9febf5f8b2f080fd6
Connor Claxton <czclaxton@gmail.com>
2019-06-26 13:51:51 -0500

    ignore

--- index.js
+++ index.js
@@ -24,23 +24,3 @@ const tabContentImg = document.querySelectorAll(".teamImg");
 tabContentImg.addEventListener("click", img =>
   img.classList.toggle("animated flipInX")
 );
-
-function nameFilter() {
-  // Declare variables
-  var input, filter, devDiv, devName, a, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  devDiv = document.getElementsByClassName("devTeamMemb");
-  devName = devDiv.getElementsByTagName("li");
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
